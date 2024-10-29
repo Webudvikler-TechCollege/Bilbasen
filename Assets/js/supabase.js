1d0f2efc84f5765b4a89bd424737e5749b9abedf
Webudvikler Hovedforløb <heka@techcollege.dk>
2024-10-29 12:33:51 +0100

    Comments

--- Assets/js/supabase.js
+++ Assets/js/supabase.js
@@ -1,4 +1,6 @@
+/* Henter createClient fra CDN */
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+/* Importerer Supabase credentials */
 import { supabaseUrl, supabaseKey } from './credentials.js'
-
+// Deklarerer supabase client object
 export const supabase = createClient(supabaseUrl, supabaseKey)
