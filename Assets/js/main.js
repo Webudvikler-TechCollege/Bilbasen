a394d499b7cb24b6a67f86d6224f1006aafb2635
Webudvikler Hovedforløb <heka@techcollege.dk>
2024-11-05 08:46:58 +0100

    Remove credentials

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-const supabaseUrl = 'https://avrkzifxylsaixsjdumi.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2cmt6aWZ4eWxzYWl4c2pkdW1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkzMTQ5NzMsImV4cCI6MjA0NDg5MDk3M30.NrWx8Cgid-Li1b0ejtOFN4HKLEN0XdQHtT-mIkAKrpg'
+const supabaseUrl = ''
+const supabaseKey = ''
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
 export const fetchData = async () => {
